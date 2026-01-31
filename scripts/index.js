9e8a88a9aaa3b9c25e14a1d586f52462c5d3ea5d
Bob <eehab47@outlook.com>
2026-01-31 12:39:58 -0800

    fix figame and open model

--- scripts/index.js
+++ scripts/index.js
@@ -23,32 +23,32 @@ const profileDescriptionEl = document.querySelector(".profile__description");
 
 newPostFormElement.addEventListener("submit", (evt) => {
   evt.preventDefault();
-
+  console.log(newPostImageInput.value, editCaptionInput.value);
   newPostModal.classList.remove("modal_is-opened");
 });
 editProfileBtn.addEventListener("click", function () {
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileDescriptionInput.value = profileDescriptionEl.textContent;
-  editProfileModal.classList.add("modal__is-opened");
+  editProfileModal.classList.add("modal_is-opened");
 });
 
 editProfileCloseBtn.addEventListener("click", function () {
-  editProfileModal.classList.remove("modal__is-opened");
+  editProfileModal.classList.remove("modal_is-opened");
 });
 
 newPostBtn.addEventListener("click", function () {
-  newPostModal.classList.add("modal__is-opened");
+  newPostModal.classList.add("modal_is-opened");
 });
 
 newPostCloseBtn.addEventListener("click", function () {
-  newPostModal.classList.remove("modal__is-opened");
+  newPostModal.classList.remove("modal_is-opened");
 });
 
 function handleEditProfileSubmit(evt) {
   evt.preventDefault();
   profileNameEl.textContent = editProfileNameInput.value;
   profileDescriptionEl.textContent = editProfileDescriptionInput.value;
-  editProfileModal.classList.remove("modal__is-opened");
+  editProfileModal.classList.remove("modal_is-opened");
   console.log("submitting");
 }
 
